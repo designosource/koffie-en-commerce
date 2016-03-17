940625b6e6e7ef707f3c308b393be0e306ad9a45
Ian Emsens <ianemsens@gmail.com>
2016-03-17 12:33:27 +0100

    testing change

--- koffie-en-commerce/resources/views/welcome.blade.php
+++ koffie-en-commerce/resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Koffie en Commerce</div>
+                <div class="title">Koffie en Commerce.</div>
             </div>
         </div>
     </body>
