32ac586b6a944e0cad850b3a6ee4e597d32b6468
Marijn Rutten <Marijn@Marijns-MacBook-Pro.local>
2016-03-21 13:07:12 +0100

    added migration for video

---
+++ koffie-en-commerce/database/migrations/2016_03_21_113757_tbl_video.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class TblVideo extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tbl_video', function (Blueprint $table) {
+            $table->increments('video_id');
+            $table->integer('video_category_id')->foreign('video_category_id')->references('category_id')->on('tbl_category');
+            $table->string('video_title');
+            $table->string('video_description')->unique();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('tbl_video');
+    }
+}
