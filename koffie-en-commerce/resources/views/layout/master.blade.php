5b5f5ecd4ae09a00ba89ffcc901ecc21453d0aa5
Ian Emsens <ianemsens@gmail.com>
2016-03-27 13:55:08 +0200

    fuck this entire setup

--- koffie-en-commerce/resources/views/layout/master.blade.php
+++ koffie-en-commerce/resources/views/layout/master.blade.php
@@ -4,6 +4,7 @@
 	<meta charset="UTF-8">
 	<title>K&C - @yield('title')</title>
 	@yield('seo')
+	<link rel="stylesheet" type="text/css" href="../koffie-en-commerce/public/css/app.css">
 </head>
 <body>
 	@yield('content')
