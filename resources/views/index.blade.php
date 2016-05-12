1395c56b35042e8f7fa3015b5496b6fe3c93c5d9
Nickvanpuyvelde <nick@marbles.be>
2016-05-12 17:11:35 +0200

    Add a background image above the fold and the introduction tekst

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,17 @@
 
 @section('content')
     <div class="container">
+        <div class="bgimage">{!! Html::image('image/imageheader.jpg') !!}
+          <h1>Verhalen over de zakelijke kant van media</h1>
+</div>
+      <section class="introduction">
+        <div class="wrapp">
+        <h2>Koffie &amp; Commerce</h2>
+        <p>Welkom bij Koffie &amp; Commerce. Dè bron van inspirerende verhalen en informatie over de zakelijke kant van media. Inhoud creëeren is uitdagend op zich, maar er centen mee verdienen is vaak nog het lastigste van al.
+Ontdek de verschillende navigeermogelijkheden om de onderwerpen, verhalen en media-experten waar jouw interesse naar uitgaat te exploreren.
+Een film- of TV-project financieren? Advertentie inkomsten uit digitale publicaties? U wagen aan branded content? En wat met rechten of internationalisatie? … Behalve boeiende clips over deze onderwerpen waar je met een koffietje rustig kan naar kijken, vind je telkens ook de nodige bronnen en informatie om zelf aan de slag te gaan met de onderwerpen die door de experten aangehaald worden.</p>
+      </div>
+    </section>
         <h1>Home</h1>
         <ul>
             <li>
