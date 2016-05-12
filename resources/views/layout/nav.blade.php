98f61416564b16f135db7f956793918739bdea7e
Nickvanpuyvelde <nick@marbles.be>
2016-05-12 16:06:43 +0200

    Add a basic navigation and some styling to that navigation

--- resources/views/layout/nav.blade.php
+++ resources/views/layout/nav.blade.php
@@ -1,3 +1,4 @@
-<nav>
-    I am the Navigation
-</nav> 
+<nav class="mainnavigation">
+    <a href="{{action('HomeController@index')}}" class="home">{!! Html::image('image/logo_nav.png') !!}</a>
+    <a href="#" class="menu">Menu</a>
+</nav>
