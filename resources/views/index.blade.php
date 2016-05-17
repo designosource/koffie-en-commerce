96416eb363834f0413d1009ec74f2c8edd2ac029
Nickvanpuyvelde <nick@marbles.be>
2016-05-17 10:52:39 +0200

    Add styling above the fold on the homepage

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,8 @@
 
 @section('content')
     <div class="container">
-        <div class="bgimage">{!! Html::image('image/imageheader.jpg') !!}
+        <div class="bgimage">{!! Html::image('image/imageheader.jpg' , 'header image', array('class' => 'header_image'))!!}
+          {!! Html::image('image/logo_nav.png' , 'Logo above the fold', array('class' => 'headerlogo'))!!}
           <h1>Verhalen over de zakelijke kant van media</h1>
 </div>
       <section class="introduction">
@@ -12,9 +13,10 @@
         <h2>Koffie &amp; Commerce</h2>
         <p>Welkom bij Koffie &amp; Commerce. Dè bron van inspirerende verhalen en informatie over de zakelijke kant van media. Inhoud creëeren is uitdagend op zich, maar er centen mee verdienen is vaak nog het lastigste van al.
 Ontdek de verschillende navigeermogelijkheden om de onderwerpen, verhalen en media-experten waar jouw interesse naar uitgaat te exploreren.
-Een film- of TV-project financieren? Advertentie inkomsten uit digitale publicaties? U wagen aan branded content? En wat met rechten of internationalisatie? … Behalve boeiende clips over deze onderwerpen waar je met een koffietje rustig kan naar kijken, vind je telkens ook de nodige bronnen en informatie om zelf aan de slag te gaan met de onderwerpen die door de experten aangehaald worden.</p>
+Een film- of TV-project financieren? Advertentie inkomsten uit digitale publicaties? U wagen aan branded content? En wat met rechten of internationalisatie? … </p><p>Behalve boeiende clips over deze onderwerpen waar je met een koffietje rustig kan naar kijken, vind je telkens ook de nodige bronnen en informatie om zelf aan de slag te gaan met de onderwerpen die door de experten aangehaald worden.</p>
       </div>
     </section>
+    <!--
         <h1>Home</h1>
         <ul>
             <li>
