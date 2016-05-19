fc59c2860fc0acdcc3dcc9b9c1ca0a405d5d2a7b
Jasper De Smet <jasp.desmet@gmail.com>
2016-05-19 14:40:45 +0200

    Fix video controller

--- resources/views/video/index.blade.php
+++ resources/views/video/index.blade.php
@@ -6,13 +6,22 @@
 	<div class="container">
 		<h1>Videos</h1>
 		<a href="{{action('PageController@index')}}">
-            <b>Home</b> /
-        </a>
-        <br>
-
-        @foreach ($categories as $category)
-			<br/><a href="{{action('VideoController@category',['slug' => $category->slug])}}">{{$category->name}}</a>
-		@endforeach
-
+			<b>Home</b> /
+		</a>
+		<br>
+	
+		<div>
+			<h2>Categories</h2>
+			@foreach ($categories as $category)
+				<br/><a href="{{action('VideoController@category',['slug' => $category->slug])}}">{{$category->name}}</a>
+			@endforeach
+		</div>
+	
+		<div>
+			<h2>VIDEO'S</h2>
+			@foreach ($videos as $video)
+				{{dump($video)}}
+			@endforeach
+		</div>
 	</div>
 @endsection
