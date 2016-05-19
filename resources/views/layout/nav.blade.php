3e60aa110746e314d5b6d8f50bcc4ef72d01f182
Nickvanpuyvelde <nick@marbles.be>
2016-05-19 16:56:15 +0200

    Styling above the fold

--- resources/views/layout/nav.blade.php
+++ resources/views/layout/nav.blade.php
@@ -1,4 +1,4 @@
 <nav class="mainnavigation">
     <a href="{{action('PageController@index')}}" class="home">{!! Html::image('image/headerlogo.png') !!}</a>
-    <a href="#" class="menu">Menu</a>
+    <a href="#" class="menu">MENU</a>
 </nav>
