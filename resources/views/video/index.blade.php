ae5a1fa4f54be6aacc156878008a033ab1e3e0c5
Nick Van Puyvelde <r0463634@student.thomasmore.be>
2016-05-23 17:43:17 +0200

    add some styling and changes to the video page

--- resources/views/video/index.blade.php
+++ resources/views/video/index.blade.php
@@ -3,21 +3,26 @@
 @section('title', 'Video Overview')
 
 @section('content')
-	<div class="container">
-		<h1>Videos</h1>
-		<a href="{{action('PageController@index')}}">
-			<b>Home</b> /
-		</a>
+	<div class="container-fluid">
+		<div class="headerimage1">
+				{!! Html::image('image/videoimage.png' , 'header image', array('class' => 'header_image1 img-responsive'))!!}
+				<header>
+						<h1 class="img-responsive" >Videos</h1>
+				</header>
 		<br>
-	
-		<div>
-			<h2>Categories</h2>
+</div>
+		<div class="tekst">
+			<p>Dè bron van inspirerende verhalen en informatie over de zakelijke kant van media. Inhoud creëeren is uitdagend op zich, maar er centen mee verdienen is vaak nog het lastigste van al.
+Ontdek de verschillende navigeermogelijkheden om de onderwerpen, verhalen en media-experten waar jouw interesse naar uitgaat te exploreren.</p>
+		</div>
+
+<div class="category">
 			@foreach ($categories as $category)
-				<br/><a href="{{action('VideoController@category',['slug' => $category->slug])}}">{{$category->name}}</a>
+				<a href="{{action('VideoController@category',['slug' => $category->slug])}}"><span class="namecat">{{$category->name}}</span></a>
 			@endforeach
 		</div>
-	
-		<div>
+
+			<div class="videos">
 			<h2>VIDEO'S</h2>
 			@foreach ($videos as $video)
 				{{dump($video)}}
