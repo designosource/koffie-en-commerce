ffca31a0e5449706fc65b695bc21da69865f8d66
thaislenglez <thaislenglez@hotmail.com>
2016-05-24 10:01:03 +0200

    navigation

--- resources/views/layout/nav.blade.php
+++ resources/views/layout/nav.blade.php
@@ -1,4 +1,24 @@
 <nav class="mainnavigation">
     <a href="{{action('PageController@index')}}" class="home">{!! Html::image('image/headerlogo.png') !!}</a>
-    <a href="#" class="menu">MENU</a>
+    
+
+
+    <!-- <a href="#" class="menu">MENU</a> -->
+
+	<div id="myNav" class="overlay">
+
+	    <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">×</a>
+
+	  <img class="logo_cup" src="image/kopje.png">
+	  <div class="overlay-content">
+	    <a href="#">HOME</a>
+	    <a href="#">ABOUT US</a>
+	    <a href="#">VIDEO'S</a>
+	    <a href="#">CONTACT</a>
+	  </div>
+
+	</div>
+
+	<span class="menu" onclick="openNav()">MENU</span>
+
 </nav>
