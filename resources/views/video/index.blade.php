09eec61fcbd440a54d87a3a759f1189c17df9030
Nick Van Puyvelde <r0463634@student.thomasmore.be>
2016-05-24 13:14:50 +0200

    fixed grid bug and background image

--- resources/views/video/index.blade.php
+++ resources/views/video/index.blade.php
@@ -5,11 +5,11 @@
 @section('content')
 	<div class="container-fluid">
 		<div class="headerlarge">
-			{!! Html::image('image/videoimage.png' , 'header image', array('class' => 'header_large img-responsive'))!!}
+			<div class="video">
 			<header>
 				<h1 class="img-responsive" >Videos</h1>
 			</header>
-			<br>
+			</div>
 		</div>
 		<div class="tekst">
 			<p>Dè bron van inspirerende verhalen en informatie over de zakelijke kant van media. Inhoud creëeren is uitdagend op zich, maar er centen mee verdienen is vaak nog het lastigste van al.
@@ -19,13 +19,13 @@ Ontdek de verschillende navigeermogelijkheden om de onderwerpen, verhalen en med
 			@foreach ($categories as $category)
 				<a href="{{action('CategoryController@category',['slug' => $category->slug])}}"><span class="namecat">{{$category->name}}</span></a>
 			@endforeach
-		</div>	
+		</div>
 		<div class="videos">
 			<h2>VIDEO'S</h2>
 			@foreach ($videos as $video)
 				{{dump($video)}}
 				@foreach ($video->categories()->get() as $category)
-					{{dump($category)}}	
+					{{dump($category)}}
 				@endforeach
 				<hr>
 				<hr>
