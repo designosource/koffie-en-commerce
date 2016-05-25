6b31fa1cc7b57d16218d56bd53fbd58b79b18fee
Marijn Rutten <Marijn@Marijns-MacBook-Pro.local>
2016-05-25 15:06:41 +0200

    Video controller and view

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -27,11 +27,13 @@ class VideoController extends Controller
     public function detail($slug){
 
         // Get video
-        $video = \App\Entity\Video::where('slug','=', $slug)->first();
+        $video = \App\Entity\Video::where('slug','=', $slug)->get();
+        $videosSpeaker = \App\Entity\Video::where('speaker_id','=', $video[0]->speaker_id)->where("id","<>", $video[0]->id)->limit(3)->get();
 
         return view('video/detail',[
-            'video' => $video,
-            'speaker' => $video->speaker
+            'video' => $video[0],
+            'videosSpeaker' => $videosSpeaker,
+            'speaker' => $video[0]->speaker,
         ]);
     }
 }
