e96b5895c643b4e50016e5656fc66bfd2176608f
thaislenglez <thaislenglez@hotmail.com>
2016-05-31 00:12:49 +0200

    route about, above the fold

--- resources/views/layout/nav.blade.php
+++ resources/views/layout/nav.blade.php
@@ -5,11 +5,18 @@
 		<a href="javascript:void(0)" class="mainnavigation-overlay-closebtn" onclick="closeNav()">×</a>
 		<img class="mainnavigation-overlay-logocup" src="image/kopje.png">
 		<div class="mainnavigation-overlay-content">
-		    <a href="#">HOME</a>
-		    <a href="#">ABOUT US</a>
-		    <a href="#">VIDEO'S</a>
-		    <a href="#">CONTACT</a>
+		    <a href="/">HOME</a>
+		    <a href="/over-ons">ABOUT US</a>
+		    <a href="/videos">VIDEO'S</a>
+		    <a href="/contact">CONTACT</a>
 	  	</div>
 	</div>
 	<span class="mainnavigation-menu" onclick="openNav()">MENU</span>
 </nav>
+
+
+
+
+<!-- 			{!!
+				link_to('../', 'Home', ['class' => 'nav-item-link'])
+			!!} -->
--- resources/views/page/about.blade.php
+++ resources/views/page/about.blade.php
@@ -1,1 +1,19 @@
-<h1>Hello about</h1>
+@extends('layout.master')
+
+@section('title', 'Over ons')
+
+@section('content')
+
+<div class="container-fluid"> 
+    <div class="about">
+        <h1>Over ons</h1>
+    </div>
+</div>
+
+<section class="copy">
+  <p>Voel je je geïnspireerd door de getuigenissen en onderwerpen van Koffie &amp; Commerce?<br>
+    Wil je meer weten of misschien samen een probleem onder de loep nemen in verband met de zakelijke kant van media? <br>Vul dan onderstaand formulier in en je komt rechtstreeks in contact met de Thomas More experts More achter dit initiatief.
+  </p>
+</section>
+
+@endsection
