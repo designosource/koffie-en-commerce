e395e6c3f000916311fc7cc7cae0357d4af81733
Ian Emsens <ianemsens@gmail.com>
2016-05-31 13:07:19 +0200

    last of front page responsification

--- resources/views/layout/quote.blade.php
+++ resources/views/layout/quote.blade.php
@@ -2,8 +2,6 @@
     {!! Html::image('image/quote.png' , 'quote', array('class' => 'quotesign img-responsive'))!!}
     <p class="citaat">Dè bron van inspirerende verhalen en informatie over de zakelijke kant van media</p>
     <div class="persooncitaat">
-        {!! Html::image('image/line.png' , 'lineleft', array('class' => 'lineleft img-responsive'))!!}
         <p class="name">Rutger Becker</p>
-        {!! Html::image('image/line.png' , 'lineright', array('class' => 'lineright img-responsive'))!!}
     </div>
 </section>
