0940bba5591a90eb2e8bfbb6777d959d431ff7ea
Sara Barrera Riano <sarabarrera.riano@gmail.com>
2016-06-03 15:49:00 +0200

    no message

--- gulpfile.js
+++ gulpfile.js
@@ -17,42 +17,44 @@ var gulp = require('gulp'),
     sass = require('gulp-sass'), // development & production
     autoprefixer = require('gulp-autoprefixer'), // development & production
     notify = require('gulp-notify'), // development & production
-    jshint = require('gulp-jshint'); // dev
+    jshint = require('gulp-jshint'), // dev
+    livereload = require('gulp-livereload');
 
 /**
  * SASS COMPILING: development
  */
-gulp.task('sass', function() {
+gulp.task('sass', function () {
     gulp.src('resources/assets/scss/**/*.scss')
         .pipe(sass({
             'outputStyle': 'expanded',
             'sourceComments': true,
             'lineNumbers': true
-        }).on('error', function(err) {
+        }).on('error', function (err) {
             console.error('Sass error: ', err.message);
         }))
         .pipe(autoprefixer({
             browsers: ['last 3 versions']
         }))
         .pipe(gulp.dest('public/css'))
-        .pipe(notify({ message: 'Styles task complete' }));
+        .pipe(notify({message: 'Styles task complete'}));
 });
 
 /**
  * JS linter
  */
-gulp.task('lint', function() {
-  return gulp.src(sourceDir + 'js/**/*.js')
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'));
+gulp.task('lint', function () {
+    return gulp.src(sourceDir + 'js/**/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
 });
 
 /**
  * Watch for changes | Executes sass and jshint
  */
-gulp.task('watch', function(){
+gulp.task('watch', function () {
 
     // Watch for sass changes
+    livereload.listen('start');
     gulp.watch('resources/assets/scss/**/*.scss', ['sass']);
 
     // Watch for js changes
