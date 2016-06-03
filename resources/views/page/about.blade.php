f7909585cb23a4579d5627b1cd029bb9d8e366a5
thaislenglez <thaislenglez@hotmail.com>
2016-06-03 14:09:53 +0200

    about us fix

--- resources/views/page/about.blade.php
+++ resources/views/page/about.blade.php
@@ -4,11 +4,11 @@
 
 @section('content')
 
-<div class="container-fluid"> 
     <div class="about">
-        <h1>Over ons</h1>
+    	<header>
+        	<h1>Over ons</h1>
+        </header>
     </div>
-</div>
 
 <section class="copy">
   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eu orci a enim vestibulum viverra quis ut tellus. Aliquam eleifend venenatis tempor. Vivamus id semper odio, non pretium turpis. Fusce a elit sit amet ex malesuada porttitor in non massa. Aliquam eget metus odio. Nam consectetur cursus eros vel ullamcorper. Nunc eleifend eu nunc vel vulputate. Integer ac neque condimentum, condimentum erat eget, egestas nisi.
