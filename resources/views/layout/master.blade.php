8c8f0c579c19649f5829b78bee91dee8d6dc55e8
Ian Emsens <ianemsens@gmail.com>
2016-06-03 15:44:55 +0200

    fixed inclusion error

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
     <link rel="stylesheet" href="{{ URL::asset('css/style.css') }}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    @include(layout.favicon)
+    @include('layout.favicon')
 </head>
 
 <body class="user-body">
