524edd171f54f37e444556f55c8a1c4a46ba7546
Ian Emsens <ianemsens@gmail.com>
2016-06-03 15:39:27 +0200

    admin raw setup

--- resources/views/admin/video/index.blade.php
+++ resources/views/admin/video/index.blade.php
@@ -1,29 +1,24 @@
-@extends('layout.master')
+@extends('layout.admin')
 
 @section('title', 'Video Overview')
 
 @section('content')
-	<div class="container admin">
-		<h1>ADMIN DASHBOARD</h1>
-		<div>
-			<h2>VIDEO'S</h2>
-			<div class="row">
-				@foreach ($videos as $video)
-					<div class="col-md-4 admin-video">
-						<a href="{{route('admin_videos_edit', $video->id)}}" class="video-thumb">
-							<img src="{{ $video->vimeo_thumb != "" ? $video->vimeo_thumb : 'https://placeholdit.imgix.net/~text?txtsize=60&txt=640%C3%97360&w=640&h=360' }}" alt="" id="thumb-{{$video->vimeo}}">
-						</a>
-						<div>
-							<h3 class="title"><a href="{{route('admin_videos_edit', $video->id)}}">{{$video->title}}</a></h3>
-							<div class="categories">
-								@foreach ($video->categories as $category)
-									<a href="#" class="category-item">{{$category->name}}</a>
-								@endforeach
-							</div>
-						</div>						
+	<h2>Video's</h2>
+	<div class="row">
+		@foreach ($videos as $video)
+			<div class="col-md-4 admin-video">
+				<a href="{{route('admin_videos_edit', $video->id)}}" class="video-thumb">
+					<img src="{{ $video->vimeo_thumb != "" ? $video->vimeo_thumb : 'https://placeholdit.imgix.net/~text?txtsize=60&txt=640%C3%97360&w=640&h=360' }}" alt="" id="thumb-{{$video->vimeo}}">
+				</a>
+				<div>
+					<h3 class="title"><a href="{{route('admin_videos_edit', $video->id)}}">{{$video->title}}</a></h3>
+					<div class="categories">
+						@foreach ($video->categories as $category)
+							<a href="#" class="category-item">{{$category->name}}</a>
+						@endforeach
 					</div>
-				@endforeach
+				</div>						
 			</div>
-		</div>
+		@endforeach
 	</div>
 @endsection
---
+++ resources/views/layout/admin.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>K&C - @yield('title')</title>
+    @yield('seo')
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{ URL::asset('css/style.css') }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    @include('layout.favicon')
+</head>
+
+<body class="admin-body">
+
+<script src="{{ URL::asset('js/jquery-1.12.4.min.js') }}"></script>
+<script src="{{ URL::asset('js/vimeo-thumb.js') }}"></script>
+    @include('layout.adminNav')
+    <div class="container-fluid admin-nav-compensation">
+        <div class="row">
+            <div class="col-sm-3 col-md-2 sidebar">
+                <ul class="nav nav-sidebar">
+                    <li><a href="/admin/videos">Video Overview</a></li>
+                    <li><a href="/admin/videos/create">New Video</a></li>
+                </ul>
+            </div>
+            <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+                @yield('content')
+            </div>
+        </div>
+    </div>
+</body>
+
+@yield('scripts')
+</html>
