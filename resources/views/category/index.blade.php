d64dd4b9846575aae64dc7535a7635f94765205f
Ian Emsens <ianemsens@gmail.com>
2016-06-06 15:48:54 +0200

    created gallery styling

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -3,35 +3,39 @@
 @section('title', 'Category Overview')
 
 @section('content')
-<style media="screen">
-  .categorien{
-    background-image:url("http://placehold.it/350x150");
-  }
-</style>
-<div class="container-fluid banner">
-  <div class="headersmall">
+
+  <div class="container-fluid banner">
+    <div class="headersmall">
       <div class="categorien">
         <header>
           <h1 class="categorieen__title">{{$category->name}}</h1>
-  			</header>
+        </header>
       </div>
-  </div>
-<div class="catego-info">
-<p class="catego-info__description">{{$category->description}}</p>
-</div>
-<div class="filter">
+    </div>
+    <div class="catego-info">
+      <p class="catego-info__description">{{$category->description}}</p>
+    </div>
+    <div class="filter">
       <a href="#"><span class="filter--name">subfilter1</span></a>
       <a href="#"><span class="filter--name">subfilter2</span></a>
       <a href="#"><span class="filter--name">subfilter3</span></a>
       <a href="#"><span class="filter--name">subfilter4</span></a>
-</div>
-        @foreach ($videos as $video)
-            <p>This is video <a href="{{action('VideoController@detail',['id' => $video->slug])}}"><b>{{$video->title}}</b></a></p>
-            <p>Their short description is <b>{{$video->short_description}}</b></p>
-            <p>Their long description is <b>{{$video->long_description}}</b></p>
-            <p>category</p>
-            {{dump($video->categories())}}
-            <hr>
-        @endforeach
     </div>
+  </div>
+  
+  <section class="gallery clearfix">
+    @foreach ($videos as $video)
+    <a href="/videos/{{$video->slug}}">
+      <div class="gallery-item col-xs-6 col-sm-4 col-lg-3" style=" background-image: url('{{$video->vimeo_thumb}}')" >
+        <div class="content">
+          <div class="gallery-item-description">
+            <h2>{{$video->title}}</h2>
+            <p>{{$video->short_description}}</p>
+          </div>
+        </div>
+      </div>
+    </a>
+    @endforeach
+  </section>
+
 @endsection
