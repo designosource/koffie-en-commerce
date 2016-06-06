d051eeec16ecbfa9632b7dd16a07074845f0663d
Ian Emsens <ianemsens@gmail.com>
2016-06-06 14:53:48 +0200

    work on admin page

--- resources/views/admin/video/index.blade.php
+++ resources/views/admin/video/index.blade.php
@@ -4,21 +4,24 @@
 
 @section('content')
 	<h2>Video's</h2>
-	<div class="row">
+	<ul class="admin-list clearfix">
 		@foreach ($videos as $video)
-			<div class="col-md-4 admin-video">
-				<a href="{{route('admin_videos_edit', $video->id)}}" class="video-thumb">
-					<img src="{{ $video->vimeo_thumb != "" ? $video->vimeo_thumb : 'https://placeholdit.imgix.net/~text?txtsize=60&txt=640%C3%97360&w=640&h=360' }}" alt="" id="thumb-{{$video->vimeo}}">
+			<li class="col-xs-12 admin-list-row">
+				<a href="{{route('admin_videos_edit', $video->id)}}" class="video-thumb col-xs-2">
+					<img src="{{ $video->vimeo_thumb != "" ? $video->vimeo_thumb : 'https://placehold.it/640x360' }}" alt="{{$video->title}}" id="thumb-{{$video->vimeo}}">
 				</a>
-				<div>
-					<h3 class="title"><a href="{{route('admin_videos_edit', $video->id)}}">{{$video->title}}</a></h3>
+				<div class="col-xs-10">
+					<h3 class="title"><a href="{{route('admin_videos_edit', $video->id)}}">{{$video->title}}</a><small>by&nbsp;{{$video->speaker->name}}</small></h3>
+					<div class="description">
+						{{$video->long_description}}
+					</div>
 					<div class="categories">
 						@foreach ($video->categories as $category)
 							<a href="#" class="category-item">{{$category->name}}</a>
 						@endforeach
 					</div>
 				</div>						
-			</div>
+			</li>
 		@endforeach
-	</div>
+	</ul>
 @endsection
