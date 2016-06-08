661e63c96ee05c9b476beaea9442abdec5c5022a
Ian Emsens <ianemsens@gmail.com>
2016-06-08 23:22:14 +0200

    initial admin styling completed

--- resources/views/admin/video/createOrUpdate.blade.php
+++ resources/views/admin/video/createOrUpdate.blade.php
@@ -3,46 +3,61 @@
 @section('title', 'New Video')
 
 @section('content')
-	{{ Form::model($video, array('route' =>	$action), 'Selecteer een category') }}
-		<div>
+	{{ Form::model($video, array('route' =>	$action, 'class' => 'col-xs-12'), 'Selecteer een category') }}
+	<div class="col-xs-12 col-md-6">
+		<div class="input-group">
 			{{ Form::label('title', 'Titel:', array('class' => 'address')) }}
-			{{ Form::text('title') }}
+			{{ Form::text('title', '', array('class' => 'form-control')) }}
+			<small class="text-muted">De hoofding van de video.</small>
 		</div>
 
-		<div>
-			{{ Form::label('slug', 'slug:', array('class' => 'address')) }}
-			{{ Form::text('slug') }}
+		<div class="input-group">
+			{{ Form::label('slug', 'Slug:', array('class' => 'address')) }}
+			{{ Form::text('slug', '', array('class' => 'form-control')) }}
+			<small class="text-muted">De leesbare hyperlink van de video.<br/>bv .../videos/<b>example-url</b></small>
 		</div>
 
-		<div>
+		<div class="input-group">
+			{{ Form::label('vimeo', 'Vimeo URL:', array('class' => 'address')) }}
+			{{ Form::text('vimeo', '', array('class' => 'form-control')) }}
+			<small class="text-muted">Het unieke nummer van de video op Vimeo.<br/>bv vimeo.com/<b>123456</b></small>
+		</div>
+
+		<div class="input-group">
+			{{ Form::label('speaker', 'Spreker:', array('class' => 'address')) }}
+			{{ Form::select('speaker', $speakerList, $video->speaker_id, array('class' => 'form-control')) }}
+			<small class="text-muted">De spreker in deze video.</small>
+		</div>
+
+		<div class="tag-list">
+			@foreach ($categories as $category)
+				<div class="tag">
+					{{ Form::checkbox('categories[]', $category->id, in_array($category->id, $video->categories->pluck('id')->all()), ['id'=>'category-' . $category->id]) }}
+					{{ Form::label('category-' . $category->id, $category->name) }}
+				</div>
+			@endforeach
+			<small class="text-muted">De categorieën waartoe deze video toebehoort.</small>
+		</div>
+	</div>
+
+	<div class="col-xs-12 col-md-6">
+		<div class="input-group">
 			{{ Form::label('short_description', 'Korte beschrijving:', array('class' => 'address')) }}
-			{{ Form::textarea('short_description') }}
+			{{ Form::textarea('short_description', '', array('class' => 'form-control')) }}
+			<small class="text-muted">Instructions</small>
 		</div>
 		
-		<div>
+		<div class="input-group">
 			{{ Form::label('long_description', 'Lange beschrijving:', array('class' => 'address')) }}
-			{{ Form::textarea('long_description') }}
+			{{ Form::textarea('long_description', '', array('class' => 'form-control')) }}
+			<small class="text-muted">Instructions</small>
 		</div>
+	</div>
 
-		<div>
-			{{ Form::label('vimeo', 'Video URL:', array('class' => 'address')) }}
-			{{ Form::text('vimeo') }}
+	<div class="col-xs-12">
+		<div class="input-group">
+			{{ Form::submit('Opslaan', array('class'=>'button')) }}
 		</div>
-
-		<div>
-			{{ Form::label('speaker', 'Speaker:', array('class' => 'address')) }}
-			{{ Form::select('speaker', $speakerList, $video->speaker_id) }}
-		</div>
-
-		@foreach ($categories as $category)
-			<div>
-				{{ Form::checkbox('categories[]', $category->id, in_array($category->id, $video->categories->pluck('id')->all()), ['id'=>'category-' . $category->id]) }}
-				{{ Form::label('category-' . $category->id, $category->name) }}
-			</div>
-		@endforeach
-
-		<div>
-			{{ Form::submit('Send this form!') }}
-		</div>
+	</div>
 	{{ Form::close() }}
 @endsection
--- resources/views/admin/video/index.blade.php
+++ resources/views/admin/video/index.blade.php
@@ -3,24 +3,25 @@
 @section('title', 'Video Overview')
 
 @section('content')
-	<h2>Video's</h2>
 	<ul class="admin-list clearfix">
 		@foreach ($videos as $video)
 			<li class="col-xs-12 admin-list-row">
-				<a href="{{route('admin_videos_edit', $video->id)}}" class="video-thumb col-xs-2">
-					<img src="{{ $video->vimeo_thumb != "" ? $video->vimeo_thumb : 'https://placehold.it/640x360' }}" alt="{{$video->title}}" id="thumb-{{$video->vimeo}}">
-				</a>
-				<div class="col-xs-10">
-					<h3 class="title"><a href="{{route('admin_videos_edit', $video->id)}}">{{$video->title}}</a><small>by&nbsp;{{$video->speaker->name}}</small></h3>
-					<div class="description">
-						{{$video->long_description}}
+				<a href="{{route('admin_videos_edit', $video->id)}}">
+					<div class="col-xs-2 list-thumb"
+						style="background-image:url('{{ $video->vimeo_thumb != "" ? $video->vimeo_thumb : 'https://placehold.it/640x360' }}')">
 					</div>
-					<div class="categories">
-						@foreach ($video->categories as $category)
-							<a href="#" class="category-item">{{$category->name}}</a>
-						@endforeach
+					<div class="col-xs-10">
+						<h3 class="title"><a href="{{route('admin_videos_edit', $video->id)}}">{{$video->title}}</a><small>by&nbsp;{{$video->speaker->name}}</small></h3>
+						<div class="description">
+							{{$video->long_description}}
+						</div>
+						<div class="categories">
+							@foreach ($video->categories as $category)
+								<span class="label label-primary">{{$category->name}}</span>
+							@endforeach
+						</div>
 					</div>
-				</div>						
+				</a>					
 			</li>
 		@endforeach
 	</ul>
